a8b979ccb5b3bfa78e9184079496115868a16003
tkulagina <tkulagina1974@gmail.com>
2023-07-21 12:47:43 +0300

    fix

--- src/redux/sliceContact.js
+++ src/redux/sliceContact.js
@@ -1,30 +1,3 @@
-/*import { createSlice, nanoid } from '@reduxjs/toolkit';
-
-export const sliceContact = createSlice({
-  name: 'contacts',
-  initialState: [
-    { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
-    { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
-    { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
-    { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
-  ],
-  reducers: {
-    add(state, action) {
-      state.push({
-        id: nanoid(),
-        name: action.payload.name,
-        number: action.payload.number,
-      });
-    },
-
-    remove(state, action) {
-      return state.filter(item => item.id !== action.payload);
-    },
-  },
-});
-
-export const { add, remove } = sliceContact.actions;*/
-
 import { createSlice } from '@reduxjs/toolkit';
 import persistReducer from 'redux-persist/es/persistReducer';
 import storage from 'redux-persist/lib/storage';
@@ -67,5 +40,3 @@ export const persistedContactReducer = persistReducer(
 export const { add, remove } = contactSlice.actions;
 
 export const getContacts = state => state.contacts.items;
-
-//export const contactsReducer = contactSlice.reducer;
